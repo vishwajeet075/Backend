be8487c67e6353dd179b89ba66c17d04a8229795
Vishwajeet Shinde <shindevishwajeet434@gmail.com>
2024-12-24 19:14:13 +0530

    added ngork

--- functions/app.js
+++ functions/app.js
@@ -22,7 +22,7 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 
-const gateway = new Pushgateway('http:172.23.176.1:9091');
+const gateway = new Pushgateway('https://2eb5-157-33-237-230.ngrok-free.app');
 const counter = new Counter({
     name: 'nodejs_requests_total',
     help: 'Total number of requests',
